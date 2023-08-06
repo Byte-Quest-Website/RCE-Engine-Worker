7b6c7e83fbeba1d3c17689547ffb88a84cfeb01f
FusionSid <siddheshadsv@icloud.com>
2023-08-06 15:30:08 +1200

    feat: add fully functional logger with winston

--- core/helpers/types.ts
+++ core/helpers/types.ts
@@ -1,8 +1,10 @@
-import { Connection, Channel } from "amqplib";
+import { type Logger } from "winston";
+import { type Connection, type Channel } from "amqplib";
 
 export interface IGeneralRCEWorker {
     readonly queueName: string;
     readonly connection: Connection;
     readonly channel: Channel;
+    readonly logger: Logger;
     start: () => Promise<void>;
 }
--- core/workers/general_worker.ts
+++ core/workers/general_worker.ts
@@ -1,4 +1,5 @@
 import { connect, type Connection, type Channel } from "amqplib";
+import { createLogger, transports, format, type Logger } from "winston";
 
 import { type IGeneralRCEWorker } from "@types"
 
@@ -6,22 +7,40 @@ export class GeneralRCEWorker implements IGeneralRCEWorker {
     readonly queueName: string;
     readonly connection: Connection;
     readonly channel: Channel;
+    readonly logger: Logger
 
-    constructor(queueName: string, connection: Connection, channel: Channel) {
+    constructor(queueName: string, connection: Connection, channel: Channel, logger: Logger) {
         this.queueName = queueName;
         this.connection = connection;
         this.channel = channel;
+        this.logger = logger;
     }
 
     static async init(queueName: string): Promise<GeneralRCEWorker> {
         const connection = await connect("amqp://localhost")
+
         const channel = await connection.createChannel()
         await channel.assertQueue(queueName, { durable: false })
 
-        return new GeneralRCEWorker(queueName, connection, channel)
+        const logger = createLogger({
+            format: format.combine(
+                format.timestamp(),
+                format.prettyPrint()
+            ),
+            transports: [
+                new transports.Console(),
+                new transports.File({ filename: 'worker.log' })
+            ]
+        });
+
+        logger.info("Worker Waiting For Messages!")
+
+        return new GeneralRCEWorker(queueName, connection, channel, logger)
     }
 
     async start(): Promise<void> {
+        this.logger.info("Worker Consuming Messages!")
+
         this.channel.consume(this.queueName, msg => {
             if (msg === null) {
                 return;
@@ -34,9 +53,10 @@ export class GeneralRCEWorker implements IGeneralRCEWorker {
                 return this.channel.ack(msg)
             }
 
-            console.log("Recieved:", payload)
+            this.logger.info("Recieved JSON Payload!", { jsonRecieved: payload })
 
-            setTimeout(() => { console.log("Done!"); this.channel.ack(msg) }, 4000)
+            // pretending to do work
+            setTimeout(() => { this.logger.info("Completed Task!"); this.channel.ack(msg) }, 4000)
         }, { noAck: false })
     }
 }
