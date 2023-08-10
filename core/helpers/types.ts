721c9448d5b39971ed8fdec25e2bdf145aec22bd
FusionSid <siddheshadsv@icloud.com>
2023-08-10 18:26:04 +1200

    feat: add types for job responses and input

--- core/helpers/types.ts
+++ core/helpers/types.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 import { type Logger } from "winston";
+import { type ExecException } from "child_process";
 import { type Connection, type Channel } from "amqplib";
 
 export interface IGeneralRCEWorker {
@@ -17,6 +18,25 @@ export const RunCodeJobValidator = z.object({
     jobID: z.string().uuid(),
     language: SupportedLanguages,
     code: z.string(),
+    input: z.string(),
 })
 
 export type RunCodeJob = z.infer<typeof RunCodeJobValidator>
+
+export type RunCodeInfo = {
+    codeFileSrc: string,
+    codeFileDes: string,
+    
+    runFileSrc: string,
+    runFileDes: string,
+    
+    inputFileSrc: string,
+
+    containerName: string,
+    imageName: string
+
+}
+
+export type RunCodeContainerResponse = { stdout: string, stderr: string, exitCode: number, executionTime: number, memoryKilled: boolean }
+
+export type AsyncExecPromise = { error: ExecException | null, stdout: string, stderr: string }
