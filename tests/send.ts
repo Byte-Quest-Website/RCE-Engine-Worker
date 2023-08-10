5968b07a8ef00503671d09677f45561fd92ed1bf
FusionSid <siddheshadsv@icloud.com>
2023-08-11 11:13:48 +1200

    tests: add test for rickroll lang

--- tests/send.ts
+++ tests/send.ts
@@ -1,12 +1,9 @@
+import { join } from "path";
+import { readFileSync } from "fs";
 import { connect } from "amqplib";
 import { v4 as uuidv4 } from "uuid";
-import { readFileSync } from "fs";
 
 const QUEUE_NAME = "run_code";
-
-function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
-}
 
 async function sendMessages() {
     const connection = await connect("amqp://localhost");
@@ -14,26 +11,29 @@ async function sendMessages() {
 
     await channel.assertQueue(QUEUE_NAME, { durable: false });
 
-    let data = readFileSync(
-        "/Users/sid/Desktop/Coding/Other/Programming-Problem-Website/RCE-Engine-Worker/tests/code.py"
+    let pythonCode = readFileSync(join(__dirname, "code.py")).toString();
+    let ricklangCode = readFileSync(
+        join(__dirname, "code.rickroll")
     ).toString();
-    let inp = readFileSync(
-        "/Users/sid/Desktop/Coding/Other/Programming-Problem-Website/RCE-Engine-Worker/tests/input.txt"
-    ).toString();
+    let inputString = readFileSync(join(__dirname, "input.txt")).toString();
 
-    for (let i = 0; i < 2; i++) {
-        // let options: string[] = [`print("EEE ${uuidv4()}")`, "__import__('time').sleep(5)", "while True: pass", "print('hello world')", "print(", "def e(): e()\ne()", "[_ for _ in range(1000000000)]"]
-        // let msg = JSON.stringify({ jobID: uuidv4(), language: "python", code: options[Math.floor(Math.random() * options.length)], input: inp })
-        let msg = JSON.stringify({
-            jobID: uuidv4(),
-            language: "python",
-            code: data,
-            input: inp,
-        });
-        channel.sendToQueue(QUEUE_NAME, Buffer.from(msg), { persistent: true });
-    }
+    let msg1 = JSON.stringify({
+        jobID: uuidv4(),
+        language: "python",
+        code: pythonCode,
+        input: inputString,
+    });
+    channel.sendToQueue(QUEUE_NAME, Buffer.from(msg1), { persistent: true });
 
-    console.log("Sent Message");
+    let msg2 = JSON.stringify({
+        jobID: uuidv4(),
+        language: "ricklang",
+        code: ricklangCode,
+        input: inputString,
+    });
+    channel.sendToQueue(QUEUE_NAME, Buffer.from(msg2), { persistent: true });
+
+    console.log("Sent All Messages");
     setTimeout(() => connection.close(), 1000);
 }
 
