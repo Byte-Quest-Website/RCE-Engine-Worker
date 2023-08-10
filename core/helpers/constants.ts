21c5690c8f49a6fc730ca3cbc4e70f0b452cccd6
FusionSid <siddheshadsv@icloud.com>
2023-08-10 18:26:43 +1200

    feat: create Language object with config per lang

---
+++ core/helpers/constants.ts
@@ -0,0 +1,12 @@
+import { join } from "path"
+
+export const CONFIG_DIR = join(__dirname, "..", "config")
+
+export const Languages = {
+    python: {
+        path: join(CONFIG_DIR, "python"),
+        imageName: "pyexe",
+        fileName: "/tmp/main.py",
+        runScript: "/tmp/run.sh",
+    },
+} as const;
